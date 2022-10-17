3b5695b56911f0b79b60539961b19c0ab93bbe04
YuyaHiguchi <k021g1263@g.neec.ac.jp>
2022-10-17 11:20:13 +0900

    1_4

--- main.cpp
+++ main.cpp
@@ -1,58 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>
+#include <functional>
+#include <Windows.h>
 #include <time.h>
-typedef void (*PFunc)(int*);
 
-void callback1(int* s) {
-	printf("%d秒後に実行されるよ\n", *s);
-}
-
-void setTimeoout(PFunc p, int second,int *number) {
-	// コールバック関数を呼び出す
-	Sleep(second);
-
-	p(number);
-}
-
-void gambling(int *number)
-{
-	srand(time(nullptr));
-	int randomNum = rand() % 36 + 1;
-	int number_ = *number;
-	printf("今回の目は%d\n", randomNum);
-
-	if (number_ % 2 == 0) {
-		if (randomNum % 2 == 0) {
-			printf("丁！！　当たりです！\n");
-		}
-		else
-		{
-			printf("半！！　はずれです！\n");
-		}
-	}
-	else {
-		if (randomNum % 2 == 0) {
-			printf("丁！！　はずれです！\n");
-		}
-		else
-		{
-			printf("半！！　当たりです！\n");
-		}
-	}
-
-}
-
-int main() {
+int main(int argc,const char*argv[]) {
 	printf("start\n");
 	int number = 0;
-	PFunc p;
+
+	std::function<void(int)>fx = [](int i)
+	{
+		srand(time(nullptr));
+		int randomNum = rand() % 36 + 1;
+		int number_ = i;
+		printf("今回の目は%d\n", randomNum);
+
+		if (number_ % 2 == 0) {
+			if (randomNum % 2 == 0) {
+				printf("丁！！　当たりです！\n");
+			}
+			else
+			{
+				printf("半！！　はずれです！\n");
+			}
+		}
+		else {
+			if (randomNum % 2 == 0) {
+				printf("丁！！　はずれです！\n");
+			}
+			else
+			{
+				printf("半！！　当たりです！\n");
+			}
+		}
+	};
+
+	// ユーザーの入力を受付し、数に代入
 	scanf_s("%d", &number);
 
-	p = gambling;
-	//p(&number);
-	setTimeoout(p, 3000,&number);
+	// 3秒後に実行するように
+	Sleep(3000);
 
+	// 関数を呼び出し
+	fx(number);
 
 	return 0;
 }
