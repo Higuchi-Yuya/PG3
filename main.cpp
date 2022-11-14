05753cc97aa8764aa0462a57ba82c7d9dc37d23e
YuyaHiguchi <k021g1263@g.neec.ac.jp>
2022-11-14 09:29:16 +0900

    2_1 STLとコンテナクラス

--- main.cpp
+++ main.cpp
@@ -1,54 +1,41 @@
 #include <stdio.h>
-#include <functional>
-#include <Windows.h>
-#include <time.h>
+#include <list>
+#include <iostream>
 
-
-void setTimeOut(std::function<void(int)> p, int second,int number) {
-
-	//コールバック関数
-	Sleep(second * 1000);
-
-	// 関数の実行
-	p(number);
-}
-
-int main(int argc,const char*argv[]) {
-	printf("start\n");
-	int number = 0;
-
-	std::function<void(int)>fx = [](int i)
-	{
-		srand(time(nullptr));
-		int randomNum = rand() % 36 + 1;
-		int number_ = i;
-		printf("今回の目は%d\n", randomNum);
-
-		if (number_ % 2 == 0) {
-			if (randomNum % 2 == 0) {
-				printf("丁！！　当たりです！\n");
-			}
-			else
-			{
-				printf("半！！　はずれです！\n");
-			}
+int main() {
+	std::list<const char*> Yamanote = { "Tokyo","Kanda","Akihabara","Okachimachi","Ueno",
+									  "Uguisudani","Nippori","Tabata","Komagome",
+									  "Sugamo","Otsuka","Ikebukuro","Mejiro" ,"Takadanobaba",
+									  "Shin-Okubo","Shinjuku","Yoyogi","Harajuku","Shibuya",
+									  "Ebisu","Meguro","Gotanda","Osaki","Shinagawa",
+									  "Tamachi","Hamamatsucho","Shimbashi","Yurakucho" };
+	// 出力
+	for (std::list<const char*>::iterator itr = Yamanote.begin(); itr != Yamanote.end(); ++itr) {
+		std::cout << *itr << std::endl;
+	}
+	//西日暮里を挿入
+	for (std::list<const char*>::iterator itr = Yamanote.begin(); itr != Yamanote.end(); ++itr) {
+		if (*itr == "Tabata") {
+			itr = Yamanote.insert(itr, "Nishi-Nippori");
+			++itr;
 		}
-		else {
-			if (randomNum % 2 == 0) {
-				printf("丁！！　はずれです！\n");
-			}
-			else
-			{
-				printf("半！！　当たりです！\n");
-			}
+	}
+	// 出力
+	for (std::list<const char*>::iterator itr = Yamanote.begin(); itr != Yamanote.end(); ++itr) {
+		std::cout << *itr << std::endl;
+	}
+
+	// 高輪ゲートウェイを挿入
+	for (std::list<const char*>::iterator itr = Yamanote.begin(); itr != Yamanote.end(); ++itr) {
+		if (*itr == "Tamachi") {
+			itr = Yamanote.insert(itr, "Takanawa Gateway");
+			++itr;
 		}
-	};
-
-	// ユーザーの入力を受付し、数に代入
-	scanf_s("%d", &number);
-
-	// 3秒後に実行するように
-	setTimeOut(fx, 3, number);
-
+	}
+	// 出力
+	for (std::list<const char*>::iterator itr = Yamanote.begin(); itr != Yamanote.end(); ++itr) {
+		std::cout << *itr << std::endl;
+	}
 	return 0;
+
 }
